6d9730ccc6710a06e606a0a6d8a4320b6eeca382
abstractigakis <ericstratigakis@gmail.com>
2022-08-02 16:13:13 -0400

    isolated page loading component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import { useState } from "react";
-import { Router } from "next/router";
 
 import {
   Hydrate,
@@ -11,7 +9,6 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import NProgress from "nprogress";
 import PageLoading from "@components/PageLoading";
 
 import Layout from "@components/Layout";
@@ -19,34 +16,13 @@ import Layout from "@components/Layout";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const [loading, setLoading] = useState(false);
-
-  Router.events.on("routeChangeStart", (url: any) => {
-    setLoading(true);
-    NProgress.start();
-  });
-
-  Router.events.on("routeChangeComplete", (url: any) => {
-    setTimeout(() => {
-      NProgress.done();
-      setLoading(false);
-    }, 3000);
-  });
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         {/* <SessionProvider session={session}> */}
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-            integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </Head>
-        <PageLoading loading={loading} />
+
+        <PageLoading />
         <Layout>
           <Component {...pageProps} />
         </Layout>
